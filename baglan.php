f7d448c218b85ac1fcb19e70abc660e052d5d97f
hanife3741 <hanife.ozdemir.37@gmail.com>
2015-01-15 21:26:47 +0200

    baglantı bilgileri güncellendi

--- baglan.php
+++ baglan.php
@@ -1,5 +1,5 @@
 <?php
-$b=mysql_connect ("localhost","kullanici","sifre");
+$b=mysql_connect ("localhost","kullanici1","sifre2");
 $db=mysql_select_db ('db');
 mysql_query ("SET CHARACTER SET utf8");
 ?>
